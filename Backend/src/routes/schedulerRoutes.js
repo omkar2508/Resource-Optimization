6457acc48e262c6fca19a7ab57d3aa749d586003
Omkar Dattatraya Babar <omkarbabar2508@gmail.com>
2025-12-13 14:26:43 +0530

    delete

--- Backend/src/routes/schedulerRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generateTimetable } from "../controllers/schedulerController.js";
-
-const router = Router();
-
-router.post("/generate", generateTimetable);
-
-export default router;
