634b86f8f322c9ca0471b18e93583a7ce3d595f9
Omkar Dattatraya Babar <omkarbabar2508@gmail.com>
2026-01-22 16:35:50 +0530

    nodemailer logs 1

--- Backend/src/config/nodemailer.js
+++ Backend/src/config/nodemailer.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // important for 587
+  port: 465,
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
